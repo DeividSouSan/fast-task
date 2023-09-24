c18880ec58b0ea15983668ecd395c935d19dfe8f
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-09-24 18:26:32 -0300

    update: não sei como deu certo

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const submitBtn = document.querySelector('button')
 let tasksArray = [];
 
 function renderCards(array) {
-    tasksArray.map(item => {
+    tasksArray.map((item, index) => {
         const taskCard = document.createElement('div')
         taskCard.classList.add('taskCard')
         taskCard.innerText = item.text
 
         taskCard.addEventListener('click', () => {
-            removeTask(item.id)
+            removeTask(index)
         })
 
         tasksContainer.appendChild(taskCard)
@@ -19,6 +19,7 @@ function renderCards(array) {
 }
 
 function removeTask(id) {
+    tasksContainer.innerHTML = ''
     tasksArray.splice(id, 1)
 
     localStorage.setItem('tasks', JSON.stringify(tasksArray))
@@ -26,24 +27,29 @@ function removeTask(id) {
     renderCards()
 }
 
+function addTask() {
+    if (taskInput.value){
+        console.log('O input tem valor.')
+        var newTask = {
+            "id": tasksArray.length,
+            "text": taskInput.value
+        }
+    } else {
+        console.log('O input não tem valor.')
+        throw 'Não há valor no input.'
+    }
+    tasksArray.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(tasksArray))
+}
+
 submitBtn.addEventListener('click', () => {
     tasksContainer.innerHTML = ''
     if (localStorage.getItem('tasks')) {
         tasksArray = JSON.parse(localStorage.getItem('tasks'));
-        const newTask = {
-            "id": tasksArray.length,
-            "text": taskInput.value
-        }
-        tasksArray.push(newTask);
-        localStorage.setItem('tasks', JSON.stringify(tasksArray))
-    } else {
-        const newTask = {
-            "id": tasksArray.length,
-            "text": taskInput.value
-        }
-        tasksArray.push(newTask)
-        localStorage.setItem('tasks', JSON.stringify(tasksArray))
-    }
+    } 
+
+    addTask()
+    
 
     renderCards(tasksArray)
 })
