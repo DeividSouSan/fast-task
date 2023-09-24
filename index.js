c40420ece98017fc878b7a3a2ddceb01e3e61402
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-09-24 18:16:10 -0300

    update 24/09

--- index.js
+++ index.js
@@ -3,53 +3,56 @@ const taskInput = document.querySelector('#task')
 const submitBtn = document.querySelector('button')
 
 let tasksArray = [];
+
+function renderCards(array) {
+    tasksArray.map(item => {
+        const taskCard = document.createElement('div')
+        taskCard.classList.add('taskCard')
+        taskCard.innerText = item.text
+
+        taskCard.addEventListener('click', () => {
+            removeTask(item.id)
+        })
+
+        tasksContainer.appendChild(taskCard)
+    })
+}
+
+function removeTask(id) {
+    tasksArray.splice(id, 1)
+
+    localStorage.setItem('tasks', JSON.stringify(tasksArray))
+
+    renderCards()
+}
+
 submitBtn.addEventListener('click', () => {
     tasksContainer.innerHTML = ''
-
     if (localStorage.getItem('tasks')) {
-        console.log('Já existem dados...')
-
         tasksArray = JSON.parse(localStorage.getItem('tasks'));
-
         const newTask = {
             "id": tasksArray.length,
             "text": taskInput.value
         }
-
         tasksArray.push(newTask);
-        console.log(tasksArray)
-
         localStorage.setItem('tasks', JSON.stringify(tasksArray))
     } else {
-        console.log('Ainda não existem dados...')
-
         const newTask = {
             "id": tasksArray.length,
             "text": taskInput.value
         }
-
         tasksArray.push(newTask)
-
         localStorage.setItem('tasks', JSON.stringify(tasksArray))
     }
 
-    tasksArray.map(item => {
-        const taskCard = document.createElement('div')
-        taskCard.innerText = item.text
-        taskCard.classList.add('taskCard')
-        tasksContainer.appendChild(taskCard)
-    })
+    renderCards(tasksArray)
 })
 
 window.addEventListener('load', () => {
     if (localStorage.getItem('tasks')) {
         tasksArray = JSON.parse(localStorage.getItem('tasks'))
 
-        tasksArray.map(item => {
-            const taskCard = document.createElement('div')
-            taskCard.classList.add('taskCard')
-            taskCard.innerText = item.text
-            tasksContainer.appendChild(taskCard)
-        })
+        renderCards(tasksArray)
     }
 })
+
