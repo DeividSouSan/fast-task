37249b81c1c8f7723d4a04ee6bafa83d19f12fda
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-11-02 09:57:05 -0300

    feat: info modal to show how things work

---
+++ components/InfoModal.js
@@ -0,0 +1,5 @@
+const infoText = `
+    <p>
+    O Local Storage é uma funcionalidade presente nos navegadores modernos. Ele permite que o JavaScript armazene dados no cache do navegador e  que eles possam acessados.
+    </p>
+`
