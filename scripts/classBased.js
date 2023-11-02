d73b171c84dd1c37f970cd306a2ca3196b71271e
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-11-02 09:59:20 -0300

    refactor: cleaner & soc (render func isolated)

--- scripts/classBased.js
+++ scripts/classBased.js
@@ -1,38 +1,40 @@
 import { modal } from "../components/Modal.js";
 import { TasksStructure } from "../classes/TasksStructure.js";
-import { Task } from "../classes/Task.js";
 import { LocalStorageController } from "../classes/LocalStorageController.js";
-import { renderTasks } from "../services/RenderTasks.js";
+import { Task } from "../classes/Task.js";
 
+// Functions
+function renderTasks(wrapper, tasks) {
+    let arrowIconPath = "src/images/task/tiny_plus.svg"
 
+    tasks.forEach((text, index) => {
+        const taskCard = document.createElement('section')
+        taskCard.classList.add('task-card')
+        taskCard.onclick = () => {
+            Tasks.deleteTask(index)
+            StorageController.setValues(Tasks.getTasks)
+        }
+        taskCard.innerHTML = `
+                <img src=${arrowIconPath}>    
+                <p>${text}</p>
+            `
+        wrapper.appendChild(taskCard)
+    });
+}
 // HTML Elements
 const taskWrapper = document.querySelector('.task-wrapper')
 const addTaskButton = document.querySelector('.modal-btn')
-const submitButton = modal.querySelector('button')
-
+const infoButton = document.querySelector('.info-button')
 
 // Objects
-const Tasks = new TasksStructure()
+const Tasks = new TasksStructure()  
 const StorageController = new LocalStorageController('tasks')
 
-// Ações
-Tasks.addPreviousTasks(StorageController.getValues())
+// Actions
+Tasks.addPreviousTasks(StorageController.getValues()) // Catch Tasks stored in LocalStorage
+renderTasks(taskWrapper, StorageController.getValues()) // Render Tasks stored in LocalStorage
 
-renderTasks(taskWrapper, Tasks.getTasks)
-
-// Eventos
-addTaskButton.onclick = () => document.body.appendChild(modal)
-
-submitButton.addEventListener('click', () => {
-    document.body.removeChild(modal)
-
-    let text = modal.querySelector('input').value
-    let task = new Task(text)
-
-    Tasks.addTask(task)
-
-    StorageController.setValues(Tasks.getTasks)
-
-    console.log('Up to dato:', Tasks.upToDate(StorageController.getValues(), Tasks.getTasks))
-})
-
+// Events
+const showModal = (modal) => { document.body.appendChild(modal) }
+addTaskButton.onclick = () => showModal(modal) 
+infoButton.onclick = () => showModal(modal)
